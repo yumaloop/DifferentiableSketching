9e98aac0478e83eb9ef8588fd622e7d99f339b9e
yumaloop <yumaloop@gmail.com>
2021-06-21 01:48:56 +0900

    update

--- hoge.sh
+++ hoge.sh
@@ -1,14 +1,14 @@
 python3 imageopt.py duck-rabbit-650.jpg \
 --loss LPIPSLoss \
 --seed 1234 \
---width 600 \
---points 100 \
+--width 300 \
+--lines 500 \
+--init-sigma2 1.0 \
+--final-sigma2 1.0 \
 --iters 2000 \
---init-sigma2 2.0 \
---final-sigma2 2.0 \
---lr 0.015 \
+--lr 0.01 \
 --init-raster results/hoge/init.png \
 --final-raster results/hoge/final.png \
 --init-pdf results/hoge/init.pdf \
 --final-pdf results/hoge/final.pdf \
---device cuda:0 \
+--device cuda:0
