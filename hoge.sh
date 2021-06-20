a270ec5d75d72d43789044f16983f26314f294c1
yumaloop <yumaloop@gmail.com>
2021-06-21 01:17:04 +0900

    udpate

--- hoge.sh
+++ hoge.sh
@@ -5,7 +5,7 @@ python3 imageopt.py duck-rabbit-650.jpg \
 --lines 200 \
 --init-sigma2 1.0 \
 --final-sigma2 1.0 \
---iters 2000 \
+--iters 4000 \
 --lr 0.01 \
 --init-raster results/hoge/init.png \
 --final-raster results/hoge/final.png \
